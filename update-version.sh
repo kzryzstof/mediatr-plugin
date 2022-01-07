47e333a632240a47e104d0da62ca16b3058897dc
kzryzstof <BR7ygoYD$WVf>
2022-01-06 19:50:27 -0500

    Fix the name

--- update-version.sh
+++ update-version.sh
@@ -4,21 +4,21 @@ echo "Updating the version to $1 for files in $PWD"
 
 newVersion=$1
 for pluginXmlFile in $( find ../. -name "plugin.xml" ); do
-    echo "Editing file: $pluginXmlFile $newVersion"
+  echo "Editing file: $pluginXmlFile $newVersion"
 	cmd="ed -s $pluginXmlFile <<< $'H\n,s/<version>_PLACEHOLDER_<\/version>/<version>$newVersion<\/version>/g\nw'"
 	echo $cmd
 	eval $cmd
 done
 
 for pluginXmlFile in $( find ../. -name "gradle.properties" ); do
-    echo "Editing file: $pluginXmlFile $newVersion"
+  echo "Editing file: $pluginXmlFile $newVersion"
 	cmd="ed -s $pluginXmlFile <<< $'H\n,s/PluginVersion=_PLACEHOLDER_/PluginVersion=$newVersion/g\nw'"
 	echo $cmd
 	eval $cmd
 done
 
-for pluginXmlFile in $( find ../. -name "*.Assembly.cs" ); do
-    echo "Editing file: $pluginXmlFile $newVersion"
+for pluginXmlFile in $( find ../. -name "*.AssemblyInfo.cs" ); do
+  echo "Editing file: $pluginXmlFile $newVersion"
 	cmd="ed -s $pluginXmlFile <<< $'H\n,s/AssemblyFileVersionAttribute(\"1.0.0.0\")/AssemblyFileVersionAttribute(\"$newVersion\")/g\nw'"
 	cmd="ed -s $pluginXmlFile <<< $'H\n,s/AssemblyInformationalVersionAttribute(\"1.0.0\")/AssemblyInformationalVersionAttribute(\"$newVersion\")/g\nw'"
 	cmd="ed -s $pluginXmlFile <<< $'H\n,s/AssemblyVersionAttribute(\"1.0.0.0\")/AssemblyVersionAttribute(\"$newVersion\")/g\nw'"
