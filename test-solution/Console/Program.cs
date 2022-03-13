db0573678abec409b9ddaf6e2d33ff6faaefd39f
kzryzstof <BR7ygoYD$WVf>
2022-03-13 10:54:44 -0400

    Updates the SDK version.

--- test-solution/Console/Program.cs
+++ test-solution/Console/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Console
+﻿using System.Collections.Generic;
+
+namespace Console
 {
     internal class Program
     {
+        private static readonly object _crititcalSection = new object();
+
+        private static readonly Dictionary<string, string> _criticalResource = new Dictionary<string, string>();
+        
         public static void Main(string[] args)
         {
+            //  There is no way to do this any other way around!! I can't! I can't!
+            lock (_crititcalSection)
+            {
+                _criticalResource["Hello"] = "World";
+            }
         }
     }
 }
