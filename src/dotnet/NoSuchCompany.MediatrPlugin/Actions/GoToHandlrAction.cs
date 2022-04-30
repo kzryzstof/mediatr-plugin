d8ad47ee5b2dcf06792f4728ada0d1bba88a7282
kzryzstof <BR7ygoYD$WVf>
2022-04-30 19:25:53 -0400

    Readd the action.

---
+++ src/dotnet/NoSuchCompany.MediatrPlugin/Actions/GoToHandlrAction.cs
@@ -0,0 +1,64 @@
+﻿using JetBrains.Application.DataContext;
+using JetBrains.Application.UI.Actions;
+using JetBrains.Application.UI.ActionsRevised.Menu;
+using JetBrains.Application.UI.ActionSystem.ActionsRevised.Menu;
+using JetBrains.Diagnostics;
+using JetBrains.ReSharper.Feature.Services.Navigation.ContextNavigation;
+using JetBrains.ReSharper.Psi.Files;
+using JetBrains.ReSharper.Psi.Tree;
+using NoSuchCompany.ReSharperPlugin.FindMyHandlR.Diagnostics;
+using NoSuchCompany.ReSharperPlugin.FindMyHandlR.Services;
+
+namespace NoSuchCompany.ReSharper.MediatrPlugin.Actions
+{
+	[Action("GoToHandlrAction", "Go to the HandlR")]
+	public class GoToHandlrAction : IActionWithExecuteRequirement, IExecutableAction
+	{
+		private readonly IHandlrNavigator _handlrNavigator;
+
+		public GoToHandlrAction()
+		{
+			_handlrNavigator = new HandlrNavigator(new MediatR());
+		}
+
+		private bool IsMediatrRequestSelected(IDataContext context)
+		{
+			var selectedTreeNode = context.GetSelectedTreeNode<ITreeNode>();
+
+			if (selectedTreeNode is not IIdentifier selectedIdentifier || !_handlrNavigator.IsRequest(selectedIdentifier))
+			{
+				Logger.Instance.Log(LoggingLevel.WARN, "Selected element is not an MediatR request");
+				return false;
+			}
+
+			return true;
+		}
+
+		public void Execute(IDataContext context, DelegateExecute nextExecute)
+		{
+			Guard.ThrowIfIsNull(context, nameof(context));
+
+			var selectedTreeNode = context.GetSelectedTreeNode<ITreeNode>();
+
+			if (selectedTreeNode is not IIdentifier selectedIdentifier)
+			{
+				Logger.Instance.Log(LoggingLevel.WARN, $"Selected element is not an instance {nameof(IIdentifier)}");
+				return;
+			}
+
+			_handlrNavigator.Navigate(selectedIdentifier);
+		}
+
+		public IActionRequirement GetRequirement(IDataContext dataContext)
+		{
+			return CommitAllDocumentsRequirement.TryGetInstance(dataContext);
+		}
+
+		public bool Update(IDataContext context, ActionPresentation presentation, DelegateUpdate nextUpdate)
+		{
+			Guard.ThrowIfIsNull(context, nameof(context));
+
+			return IsMediatrRequestSelected(context);
+		}
+	}
+}
