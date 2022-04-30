9a66ebf4c9e0805fca04e8deed3e80bf4ddb3b2b
kzryzstof <BR7ygoYD$WVf>
2022-04-30 12:05:04 -0400

    Plugs the navigate to handlR in the NavigateTo menu,

--- src/dotnet/NoSuchCompany.MediatrPlugin/Actions/NavigateToHandlrProvider.cs
+++ src/dotnet/NoSuchCompany.MediatrPlugin/Actions/NavigateToHandlrProvider.cs
@@ -1,31 +1,54 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using JetBrains.Application.DataContext;
+using JetBrains.Diagnostics;
 using JetBrains.ReSharper.Feature.Services.Navigation.ContextNavigation;
-using JetBrains.ReSharper.Psi.CSharp.Tree;
 using JetBrains.ReSharper.Psi.Tree;
+using NoSuchCompany.ReSharperPlugin.FindMyHandlR.Diagnostics;
+using NoSuchCompany.ReSharperPlugin.FindMyHandlR.Services;
 
 namespace NoSuchCompany.ReSharper.MediatrPlugin.Actions
 {
     [ContextNavigationProvider]
     public class NavigateToCtorProvider : INavigateFromHereProvider
     {
+        private static readonly string NullActionId = null;
+        private readonly IHandlrNavigator _handlrNavigator;
+
+        public NavigateToCtorProvider()
+        {
+            _handlrNavigator = new HandlrNavigator(new MediatR());
+        }
+
         public IEnumerable<ContextNavigation> CreateWorkflow(IDataContext dataContext)
         {
-            //var node = dataContext.GetSelectedTreeNode<ITreeNode>();
-            //var typeDeclaration = node?.GetParentOfType<IClassDeclaration>();
-            //var constructor = typeDeclaration?.ConstructorDeclarations.FirstNotNull();
+            if (false == IsMediatrRequestSelected(dataContext))
+            {
+                Logger.Instance.Log(LoggingLevel.WARN, $"Selected element is not an instance {nameof(IIdentifier)}");
+                yield break;
+            }
 
-            //if (constructor != null)
-            //{
-                yield return new ContextNavigation("Go to handlR", null, NavigationActionGroup.Other, () =>
-                {
-                    
-                });
-           // }
+            yield return new ContextNavigation("Go to handlR", NullActionId, NavigationActionGroup.Other, () =>
+            {
+                var selectedTreeNode = dataContext.GetSelectedTreeNode<ITreeNode>();
+                
+                if (selectedTreeNode is not IIdentifier selectedIdentifier)
+                    return;
+
+                _handlrNavigator.Navigate(selectedIdentifier);
+            });
+        }
+
+        private bool IsMediatrRequestSelected(IDataContext context)
+        {
+            var selectedTreeNode = context.GetSelectedTreeNode<ITreeNode>();
+
+            if (selectedTreeNode is not IIdentifier selectedIdentifier || !_handlrNavigator.IsRequest(selectedIdentifier))
+            {
+                Logger.Instance.Log(LoggingLevel.WARN, "Selected element is not an MediatR request");
+                return false;
+            }
+
+            return true;
         }
     }
 }
