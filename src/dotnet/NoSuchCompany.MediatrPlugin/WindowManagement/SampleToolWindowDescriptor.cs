fcf93a49d3178dd69563ffa13a7862a5a2dfeb08
Christophe Commeyne <christophe.commeyne@hiloenergie.com>
2023-08-03 16:49:00 -0400

    Add SampleToolWindow for plugin toolset
    Two new files added: `SampleToolWindowDescriptor.cs` and `ProjectsToolWindowClass.cs` were added to the `WindowManagement` directory. These files contain the description and functionality for a new tool window for the Mediatr Plugin. This tool window helps to better manage and organize necessary tools within the Plugin, contributing to improved user interface. Updated references in `NoSuchCompany.MediatrPlugin.Rider.csproj` file to reflect these changes.

---
+++ src/dotnet/NoSuchCompany.MediatrPlugin/WindowManagement/SampleToolWindowDescriptor.cs
@@ -0,0 +1,22 @@
+using JetBrains.Application;
+using JetBrains.Application.UI.ToolWindowManagement;
+
+namespace NoSuchCompany.ReSharperPlugin.FindMyHandlR.WindowManagement
+{    
+    [ToolWindowDescriptor
+    (
+        ProductNeutralId = "MyToolWindow",
+        Text = "Sample Tool Window",
+        Type = ToolWindowType.MultiInstance,
+        Icon = typeof(JetBrains.UI.Resources.ExtensionManagerThemedIcons.ExtensionInstall),
+        VisibilityPersistenceScope = ToolWindowVisibilityPersistenceScope.Global,
+        InitialDocking = ToolWindowInitialDocking.Right
+    )]
+    public class SampleToolWindowDescriptor : ToolWindowDescriptor
+    {
+        public SampleToolWindowDescriptor(IApplicationHost host) : base(host)
+        {
+        }
+    }
+
+}
